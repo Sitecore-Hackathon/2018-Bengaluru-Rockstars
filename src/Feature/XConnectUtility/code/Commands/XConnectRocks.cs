5337a3265661412d05027a85026363bc365ea6cc
Jisha Muthuswamy <jisha86swamy@yahoo.com>
2018-03-04 03:09:47 +0530

    Added XCOnnectRocks Command

---
+++ src/Feature/XConnectUtility/code/Commands/XConnectRocks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+using Sitecore.Data.Items;
+
+
+namespace Hackathon.Feature.XConnectUtility.Commands
+{
+    [Serializable]
+    public class XConnectRocks : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            context.Parameters.Add("height", "500");
+            context.Parameters.Add("width", "810");
+
+            Sitecore.Context.ClientPage.Start(this, "Run", context.Parameters);
+        }
+
+        public virtual void Run(ClientPipelineArgs args)
+        {
+            if (args.IsPostBack)
+                return;
+
+             ModalDialogOptions mdo = new ModalDialogOptions($"/Sitecore/Admin/XConnectDashboard.aspx")
+            {
+                Header = "XConnect Rocks dashboard",
+                Height = "500",
+                Width = "810",
+                Message = "- for todays",
+                Response = true
+            };
+            SheerResponse.ShowModalDialog(mdo);
+            args.WaitForPostBack();
+        }
+
+        public override CommandState QueryState(CommandContext context)
+        {
+            return CommandState.Enabled;
+        }
+    }
+}
