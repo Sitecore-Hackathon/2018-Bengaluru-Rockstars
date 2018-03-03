1aa8a9d1fe11675114e2447155ead5d890a8f431
Deendayal Sundaria <dsundaria@sapient.com>
2018-03-04 02:41:15 +0530

    Added Dashboard Page

---
+++ src/Feature/XConnectUtility/code/Templates.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon.Feature.XConnectUtility
+{
+    public struct Templates
+    {
+        public struct Intent
+        {
+            public static readonly ID ID = new ID("{CC5169F0-10F1-4665-8DC4-3E2A6852D52C}");
+
+            public struct Fields
+            {
+                public static readonly ID IntentText = new ID("{159148DE-ED5A-4EDB-A9B0-08139B54F6B6}");
+                public static readonly ID Goal = new ID("{37706AB9-CB22-498C-995B-0332FFD33DF9}");
+               
+            }
+        }
+    }
+}
---
+++ src/Feature/XConnectUtility/code/sitecore/admin/XConnectDashboard.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="XConnectDashboard.aspx.cs" Inherits="Hackathon.Feature.ServiceAPI.sitecore.admin.XConnectDashboard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>XConnectRocks Dashboard</h1>
+        <asp:Label ID="lblMessage" runat="server" />
+        <div>
+            <asp:ListView ID="ListView1" runat="server"></asp:ListView>
+        </div>
+    </form>
+</body>
+</html>
